2b0b6dcc1d67e79dcc91aa0891dfc42a9f36f6f8
Thibaut David <thibautdavid@icloud.com>
2021-10-01 17:06:41 +0200

    refactor(types): missing new types export

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,22 @@
 import { Strapi } from "./lib/strapi";
 
 export type {
+  PaginationByPage,
+  PaginationByOffset,
+  StoreConfig,
   StrapiAuthProvider,
   StrapiAuthenticationData,
   StrapiAuthenticationResponse,
+  StrapiBasicRequestParams,
   StrapiDefaultOptions,
   StrapiEmailConfirmationData,
   StrapiForgotPasswordData,
-  StoreConfig,
   StrapiOptions,
   StrapiRegistrationData,
+  StrapiRequestParams,
   StrapiResetPasswordData,
+  StrapiResponse,
+  StrapiUser,
 } from "./lib/types";
 
 export default Strapi;
