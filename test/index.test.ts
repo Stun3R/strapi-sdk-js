1ea69139c94ca3f804fbca45f5d59306e07727c1
Thibaut DAVID <thibautdavid@icloud.com>
2023-08-07 22:00:57 +0200

    build: switch from siroc to rollup since siroc is no longer maintained

--- test/index.test.ts
+++ test/index.test.ts
@@ -2,37 +2,39 @@ import Strapi from "../src";
 
 describe("Creation of SDK instance", () => {
   test("Basic instance", () => {
-    const strapi: any = new Strapi();
+    const strapi = new Strapi();
 
     expect(strapi).toBeInstanceOf(Strapi);
 
-    expect(Object.getOwnPropertyNames(Object.getPrototypeOf(strapi))).toEqual([
-      "constructor",
-      "request",
-      "login",
-      "register",
-      "forgotPassword",
-      "changePassword",
-      "resetPassword",
-      "sendEmailConfirmation",
-      "getProviderAuthenticationUrl",
-      "authenticateProvider",
-      "logout",
-      "find",
-      "findOne",
-      "create",
-      "update",
-      "delete",
-      "fetchUser",
-      "getToken",
-      "setToken",
-      "removeToken",
-    ]);
+    expect(
+      Object.getOwnPropertyNames(Object.getPrototypeOf(strapi)).sort()
+    ).toEqual(
+      [
+        "constructor",
+        "request",
+        "login",
+        "register",
+        "changePassword",
+        "forgotPassword",
+        "resetPassword",
+        "sendEmailConfirmation",
+        "getProviderAuthenticationUrl",
+        "authenticateProvider",
+        "logout",
+        "find",
+        "findOne",
+        "create",
+        "update",
+        "delete",
+        "fetchUser",
+        "getToken",
+        "setToken",
+        "removeToken",
+      ].sort()
+    );
 
-    expect(Object.getOwnPropertyNames(strapi)).toEqual([
-      "user",
-      "options",
-      "axios",
-    ]);
+    expect(Object.getOwnPropertyNames(strapi).sort()).toEqual(
+      ["user", "options", "axios"].sort()
+    );
   });
 });
